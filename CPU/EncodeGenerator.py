e63c7578b38d80da0e2acc46942e1c19f3f3fc7a
Trà Quang Duy <tranquangduy246@gmail.com>
2023-08-16 10:28:23 +0700

    Update EncodeGenerator.py

--- CPU/EncodeGenerator.py
+++ CPU/EncodeGenerator.py
@@ -11,4 +11,24 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
-print(len(imgList))
+    #print(path)
+print(studentIds)
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+print("Encoding Started...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print(encodeListKnown)
+print("Encoding complete")
+# Lưu file sau khi Encoding
+file = open("Encodefile.p", 'wb' )
+pickle.dump(encodeListKnownWithIds,file)
+file.close()
+print("File Saved")
